2c97aa7790131bad57060bb703b2212a24cc3420
aymen gasmi <aymengasmi25@gmail.com>
2024-05-13 15:14:38 +0100

    add rest of numbers screen items

---
+++ lib/components/item.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:toku_app/models/number.dart';
+
+class Item extends StatelessWidget {
+  const Item({super.key, required this.number});
+  final Number number;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.amber,
+      ),
+      margin: const EdgeInsets.only(left: 16, top: 16, right: 16),
+      height: 100,
+      child: Row(
+        children: [
+          Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                color: const Color(0xFFEFF1F3),
+              ),
+              child: Image.asset(number.image)),
+          Padding(
+            padding: const EdgeInsets.only(left: 16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  number.jpName,
+                  style:
+                      const TextStyle(color: Color(0xFF223843), fontSize: 16),
+                ),
+                Text(
+                  number.enName,
+                  style:
+                      const TextStyle(color: Color(0xFF223843), fontSize: 16),
+                )
+              ],
+            ),
+          ),
+          const Spacer(
+            flex: 1,
+          ),
+          const Padding(
+            padding: EdgeInsets.only(right: 16),
+            child: Icon(
+              Icons.play_arrow,
+              size: 30,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/numbers_page.dart
+++ lib/screens/numbers_page.dart
@@ -1,12 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:toku_app/components/item.dart';
+
+import '../models/number.dart';
 
 class NumbersPage extends StatelessWidget {
   const NumbersPage({super.key});
+  final List<Number> numbers = const [
+    Number(
+        image: 'assets/images/numbers/number_one.png',
+        jpName: 'ichi',
+        enName: 'one'),
+    Number(
+        image: 'assets/images/numbers/number_two.png',
+        jpName: 'Ni',
+        enName: 'two'),
+    Number(
+        image: 'assets/images/numbers/number_three.png',
+        jpName: 'San',
+        enName: 'three'),
+    Number(
+        image: 'assets/images/numbers/number_four.png',
+        jpName: 'Shi',
+        enName: 'four'),
+    Number(
+        image: 'assets/images/numbers/number_five.png',
+        jpName: 'Go',
+        enName: 'five'),
+    Number(
+        image: 'assets/images/numbers/number_six.png',
+        jpName: 'Roku',
+        enName: 'six'),
+    Number(
+        image: 'assets/images/numbers/number_seven.png',
+        jpName: 'sebun',
+        enName: 'seven'),
+    Number(
+        image: 'assets/images/numbers/number_eight.png',
+        jpName: 'hachi',
+        enName: 'eight'),
+    Number(
+        image: 'assets/images/numbers/number_nine.png',
+        jpName: 'hachi',
+        enName: 'nine'),
+    Number(
+        image: 'assets/images/numbers/number_ten.png',
+        jpName: 'hachi',
+        enName: 'ten')
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFFFFDE4),
+      backgroundColor: const Color(0xFF223843),
       appBar: AppBar(
         title: const Text(
           'Numbers',
@@ -14,42 +59,11 @@ class NumbersPage extends StatelessWidget {
         ),
         backgroundColor: const Color(0xFF223843),
       ),
-      body: Container(
-        height: 100,
-        color: Colors.amber,
-        child: Row(
-          children: [
-            Container(
-                color: const Color(0xFFEFF1F3),
-                child: Image.asset('assets/images/numbers/number_one.png')),
-            const Padding(
-              padding: EdgeInsets.only(left: 16),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'ichi',
-                    style: TextStyle(color: Color(0xFF223843), fontSize: 16),
-                  ),
-                  Text(
-                    'one',
-                    style: TextStyle(color: Color(0xFF223843), fontSize: 16),
-                  )
-                ],
-              ),
-            ),
-            const Spacer(
-              flex: 1,
-            ),
-            const Padding(
-              padding: EdgeInsets.only(right: 16),
-              child: Icon(
-                Icons.play_arrow,
-                size: 30,
-              ),
-            )
-          ],
-        ),
+      body: ListView.builder(
+        itemCount: numbers.length,
+        itemBuilder: (context, index) {
+          return Item(number: numbers[index]);
+        },
       ),
     );
   }
